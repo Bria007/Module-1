ec9504b84e6836f6245116dbfde873701e78c66a
Bria007 <chebria.anderson@yahoo.com>
2025-06-12 18:49:12 -0500

    Add files via upload

---
+++ Module 1 .py
@@ -0,0 +1,10 @@
+Python 3.13.4 (v3.13.4:8a526ec7cbe, Jun  3 2025, 21:14:54) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> print(8+7)
+15
+>>> print(8-7)
+1
+>>> print(8*7)
+56
+>>> print(8/7)
+1.1428571428571428
